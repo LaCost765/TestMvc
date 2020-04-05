dca4624ef943a9a454a0a442f4dd19ae9a5de062
Egor <lacost765123@gmail.com>
2020-04-05 13:45:29 +0300

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
